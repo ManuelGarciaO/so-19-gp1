ac9d4c20aee73c0bf682f367b5a861b6b17296db
manuel19299 <manuelobezo@gmail.com>
2019-10-01 00:41:03 +0000

    Practica 4

---
+++ xv6-public/ipc/signals.c
@@ -0,0 +1,31 @@
+
+#include <signal.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int forever = 1;
+void signalHandler(int sgn){
+    printf("Received signal: %d\n",sgn);
+    forever = 0;
+}
+int main(){
+    int pid;
+    signal(10, signalHandler);
+    pid = fork();
+    if(pid == 0){
+        //soy el hijo
+        printf("Soy el hijo\n");
+        //codigo que quiero que haga el hijo
+        while(forever){
+            printf("Puto\n");
+        }
+        printf("Salio del ciclo infinito\n");
+    }
+    else{
+        printf("Soy el proceso padre y mi hijo es %d\n",pid);
+        sleep(1);
+        kill(pid,10);
+        //codigo del padre
+        for(;;){}
+    }
+}
